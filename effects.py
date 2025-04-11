a73ad2fde1b28a524cfe1ff285f5aa1ad5cbe554
JustMahesh1 <153850342+JustMahesh1@users.noreply.github.com>
2025-04-11 19:43:09 +0530

    Update effects.py

--- effects.py
+++ effects.py
@@ -30,13 +30,13 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Add a custom footer with the copyright message
-    # Footer
-    st.markdown("""
-	<div style="display: flex; justify-content: center; align-items: center; width: 100%;>
-    <div class="footer" >
-        <p>© 2025 U Mahesh </p>
-    </div> </div>
-    """, unsafe_allow_html=True)
+# Footer
+st.markdown("""
+<div style="display: flex; justify-content: center; align-items: center; width: 100%;>
+<div class="footer" >
+<p>© 2025 U Mahesh </p>
+</div> </div>
+""", unsafe_allow_html=True)
 
 
 def adjust_image_size(image, max_width=600):
